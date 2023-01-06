71532a347a46c15b28e35536f94dc600ac96841d
lcvtech <itservices@lasallecollegevancouver.com>
2023-01-05 21:26:28 -0800

    finished vector stuff

--- Pix/Math/MathHelper.cpp
+++ Pix/Math/MathHelper.cpp
@@ -33,15 +33,15 @@ Vector3 MathHelper::Normalize(const Vector3& v)
 //sdsds
 float MathHelper::Dot(const Vector2& a, const Vector2& b)
 {
-	return 0.0f;
+	return a.x * b.x + a.y * b.y;
 }
 
 float MathHelper::Dot(const Vector3& a, const Vector3& b)
 {
-	return 0.0f;
+	return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
 Vector3 MathHelper::Cross(const Vector3& a, const Vector3& b)
 {
-	return Vector3();
+	return Vector3(a.y * b.z - b.y * a.z, a.z * b.x - b.z * a.x, a.x * b.y - b.x * a.y);
 }
