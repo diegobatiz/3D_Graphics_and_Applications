470fbf2bb145fddb457f22f9bf4daff5f1920add
diegobatiz <diegow.batiz@gmail.com>
2023-01-26 20:25:37 -0800

    Matrix4

--- Pix/Math/MathHelper.cpp
+++ Pix/Math/MathHelper.cpp
@@ -50,3 +50,40 @@ bool MathHelper::AreEqual(float a, float b)
 {
 	return abs(a - b) < 0.01f;
 }
+
+Vector3 MathHelper::TransformCoord(const Vector3& v, const Matrix4& m)
+{
+	return Vector3();
+}
+
+Vector3 MathHelper::TransformNormal(const Vector3& v, const Matrix4& m)
+{
+	return Vector3();
+}
+
+float MathHelper::Determinant(const Matrix4& m)
+{
+	return 0.0f;
+}
+
+Matrix4 MathHelper::Adjoint(const Matrix4& m)
+{
+	return Matrix4();
+}
+
+Matrix4 MathHelper::Transpose(const Matrix4& m)
+{
+	return Matrix4(
+		m._11, m._21, m._31, m._41,
+		m._12, m._22, m._32, m._42,
+		m._13, m._23, m._33, m._43,
+		m._14, m._24, m._34, m._44
+	);
+}
+
+Matrix4 MathHelper::Inverse(const Matrix4& m)
+{
+	const float determinant = Determinant(m);
+	const float invDet = 1.0f / determinant;
+	return Adjoint(m) * invDet;
+}
--- Pix/Math/MathHelper.h
+++ Pix/Math/MathHelper.h
@@ -1,17 +1,31 @@
 #pragma once
 #include "Vector2.h"
 #include "Vector3.h"
+#include "Matrix4.h"
 
 namespace MathHelper
 {
 	float MagnitudeSquared(const Vector2& v);
 	float MagnitudeSquared(const Vector3& v);
+
 	float Magnitude(const Vector2& v);
 	float Magnitude(const Vector3& v);
+
 	Vector2 Normalize(const Vector2& v);
 	Vector3 Normalize(const Vector3& v);
+
 	float Dot(const Vector2& a, const Vector2& b);
 	float Dot(const Vector3& a, const Vector3& b);
+
 	Vector3 Cross(const Vector3& a, const Vector3& b);
+
 	bool AreEqual(float a, float b);
+
+	Vector3 TransformCoord(const Vector3& v, const Matrix4& m);
+	Vector3 TransformNormal(const Vector3& v, const Matrix4& m);
+	float Determinant(const Matrix4& m);
+	Matrix4 Adjoint(const Matrix4& m);
+	Matrix4 Transpose(const Matrix4& m);
+	Matrix4 Inverse(const Matrix4& m);
+
 }
